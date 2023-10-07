d24f5734851797a1f496fa406555ed79f585daee
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-10-07 17:34:33 +0000

    project structure created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import {
+  Nav,
+  Hero,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  PopularProducts,
+  SuperQuality,
+  CustomerReviews,
+  Footer,
+} from "./sections";
 
 const App = () => (
-  <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  <div className="relative">
+    <Nav />
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      <Hero />
+    </section>
+    <section className="padding">
+      <PopularProducts />
+    </section>
+    <section className="padding">
+      <SuperQuality />
+    </section>
+    <section className="padding-x py-10">
+      <Services />
+    </section>
+    <section className="padding">
+      <SpecialOffer />
+    </section>
+    <section className="bg-pale-blue padding">
+      <CustomerReviews />
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe />
+    </section>
+    <section className="bg-black padding-x padding-t pb-8">
+      <Footer />
+    </section>
+  </div>
 );
 
 export default App;
---
+++ src/sections/index.js
@@ -0,0 +1,21 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SuperQuality from "./SuperQuality";
+import Nav from "./Nav";
+import CustomerReviews from "./CustomerReviews";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+import SpecialOffer from "./SpecialOffer";
+
+export {
+  Hero,
+  PopularProducts,
+  Services,
+  SuperQuality,
+  Nav,
+  CustomerReviews,
+  Subscribe,
+  Footer,
+  SpecialOffer,
+};
