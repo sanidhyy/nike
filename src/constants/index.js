35004ce8a0ae6966dad007e3d1eb7c8b194c0f12
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-10-08 07:18:41 +0000

    footer done

--- src/constants/index.js
+++ src/constants/index.js
@@ -144,7 +144,7 @@ export const footerLinks = [
 ];
 
 export const socialMedia = [
-  { src: facebook, alt: "facebook logo" },
-  { src: twitter, alt: "twitter logo" },
-  { src: instagram, alt: "instagram logo" },
+  { src: facebook, alt: "facebook logo", link: "https://facebook.com" },
+  { src: twitter, alt: "twitter logo", link: "https://twitter.com" },
+  { src: instagram, alt: "instagram logo", link: "https://instagram.com" },
 ];
