af87ad24d2c066dd30dfb7b92d1f9cd490fdbc80
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-10-08 06:18:08 +0000

    service cards done

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Nav from "./Nav";
 import Button from "./Button";
 import ShoeCard from "./ShoeCard";
 import PopularProductCard from "./PopularProductCard";
+import ServiceCard from "./ServiceCard";
 
-export { Nav, Button, ShoeCard, PopularProductCard };
+export { Nav, Button, ShoeCard, PopularProductCard, ServiceCard };
