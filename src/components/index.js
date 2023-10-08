b60d4d7e90294b03aaa92ea4bf92058614599949
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-10-08 06:50:11 +0000

    customers review section

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Button from "./Button";
 import ShoeCard from "./ShoeCard";
 import PopularProductCard from "./PopularProductCard";
 import ServiceCard from "./ServiceCard";
+import ReviewCard from "./ReviewCard";
 
-export { Nav, Button, ShoeCard, PopularProductCard, ServiceCard };
+export { Nav, Button, ShoeCard, PopularProductCard, ServiceCard, ReviewCard };
